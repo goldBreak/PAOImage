30cc2152438bac7645e6ea52b2218c405583f332
GaoF <1027632706@qq.com>
2018-03-06 09:57:13 +0800

    no message
    no message

---
+++ PAOImage/PAOImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PAOImage
+//
+//  Created by xsd on 2018/3/6.
+//  Copyright © 2018年 com.GF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
