448db3766503ff8e37dd9ad55813c5492bfdd160
李高锋 <gf.li@mi-me.com>
2018-03-12 07:46:28 +0800

    init demos

---
+++ PAOImage/PAOImage/ViewControllers/DemoVCs/Models/PAOItemsModel.h
@@ -0,0 +1,17 @@
+//
+//  PAOItemsModel.h
+//  PAOImage
+//
+//  Created by 李高锋 on 2018/3/11.
+//  Copyright © 2018年 com.GF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PAOItemsModel : NSObject
+
+@property (nonatomic, strong) NSString *itemName;
+@property (nonatomic, strong) NSString *itemImage;
+@property (nonatomic, strong) NSString *className;
+
+@end
---
+++ PAOImage/PAOImage/ViewControllers/DemoVCs/Models/PAOItemsModel.m
@@ -0,0 +1,13 @@
+//
+//  PAOItemsModel.m
+//  PAOImage
+//
+//  Created by 李高锋 on 2018/3/11.
+//  Copyright © 2018年 com.GF. All rights reserved.
+//
+
+#import "PAOItemsModel.h"
+
+@implementation PAOItemsModel
+
+@end
